3bbec816d71eea97aa06a05e01ea161bdbfd33e4
Govinda-Tak <govinda.tak@atmecs.com>
2024-09-10 17:59:58 +0530

    created basic structure of app.

---
+++ project/deployment/native/styles.js
@@ -0,0 +1,10 @@
+import * as style0 from "D:/2024/learning/Mendix/github/SalesManagementSystem_mendix_project/project/themesource/administration/native/main";
+import * as style1 from "D:/2024/learning/Mendix/github/SalesManagementSystem_mendix_project/project/themesource/nanoflowcommons/native/main";
+import * as style2 from "D:/2024/learning/Mendix/github/SalesManagementSystem_mendix_project/project/themesource/atlas_core/native/main";
+import * as style3 from "D:/2024/learning/Mendix/github/SalesManagementSystem_mendix_project/project/themesource/feedbackmodule/native/main";
+import * as style4 from "D:/2024/learning/Mendix/github/SalesManagementSystem_mendix_project/project/themesource/myfirstmodule/native/main";
+import * as style5 from "D:/2024/learning/Mendix/github/SalesManagementSystem_mendix_project/project/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5]);
---
+++ project/deployment/react-web/start.ps1
@@ -0,0 +1,1 @@
+C:/Program Files/Mendix/10.14.0.44044\modeler\tools\node\win-x64\node.exe C:/Program Files/Mendix/10.14.0.44044\modeler\tools\node\node_modules\rollup\dist\bin\rollup -c -w
